480499c31e50ad0b5199d32bdf93b9ea195f1b64
rommelalastra81 <rommel.alastra@gmail.com>
2026-01-22 22:22:51 +0800

    initially create other models

---
+++ src/main/java/com/alastra/vet_clinic_api/model/Appointments.java
@@ -0,0 +1,13 @@
+package com.alastra.vet_clinic_api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "appointments")
+public class Appointments {
+}
